9a7d87d5273d379081f178571f1fc3f55cd18a2f
islamdiab-stack <islamdiab967@gmail.com>
2021-07-08 06:42:53 +0200

    Edit on brand logo where i edit to brand name POS

--- resources/views/dashboard/layouts/aside.blade.php
+++ resources/views/dashboard/layouts/aside.blade.php
@@ -1,9 +1,8 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-        <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a class="brand-link" href="{{route('dashboard.index')}}">
+        <span class="brand-text font-weight-bold">POS</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,7 +10,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                {{-- <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> --}}
             </div>
             <div class="info">
                 <a href="#" class="d-block">Alexander Pierce</a>
@@ -66,7 +65,7 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('dashboard.index')}}">
-                        <i class="nav-icon fas fa-users"></i>
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
