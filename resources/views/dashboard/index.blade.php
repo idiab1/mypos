4cb985c7003ea7fda2e5e985fc8e68779a60541f
islamdiab-stack <islamdiab967@gmail.com>
2021-07-08 09:40:17 +0200

    Add page name

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,6 +5,18 @@
     {{__('Home page')}}
 @endsection
 
+
+{{-- Page name --}}
+@section('page_name')
+    {{__('Dashboard')}}
+@endsection
+
+{{-- Breadcrumb Item
+@section('breadcrumb-item')
+    <li class="breadcrumb-item active"><a href="{{route('users.index')}}">Users</a></li>
+@endsection --}}
+
+
 @section('content')
     <div class="home-page">
         <h2>This is dashboard</h2>
