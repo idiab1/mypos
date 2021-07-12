3f903203fb890230ecc8c91c88baba6cab63c0a7
islamdiab-stack <islamdiab967@gmail.com>
2021-07-12 19:07:52 +0200

    Add new routes for categories then add parameters, and names for other routs

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('dashboard')->group(function () {
+Route::prefix('dashboard')->middleware(['auth'])->group(function () {
 
     // Home of dashboard routes
     Route::get('/home', 'DashboardController@index')->name('dashboard.index');
@@ -19,5 +19,18 @@ Route::prefix('dashboard')->group(function () {
         'edit'      => 'user.edit',
         'update'    => 'user.update',
         'destroy'   => 'user.destroy',
+    ]);
+    //  Categories Routes
+    Route::resource('categories', "CategoryController")->except([
+        'show',
+    ])->parameters([
+        "categories" => 'id'
+    ])->names([
+        'index'     => 'categories.index',
+        'create'    => 'category.create',
+        'store'     => 'category.store',
+        'edit'      => 'category.edit',
+        'update'    => 'category.update',
+        'destroy'   => 'category.destroy',
     ]);
 }); // End of dashboard routes
