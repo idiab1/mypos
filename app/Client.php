d38fa71baeb67a86048a7395ad8e4a69f46d34a7
islamdiab-stack <islamdiab967@gmail.com>
2021-07-14 16:28:21 +0200

    create fillable array for add all column in it, and change data type of phone to array

---
+++ app/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $guarded = [];
+    protected $casts = [
+        'phone' => 'array'
+    ];
+
+    protected $fillable = [
+        'name', 'address'
+    ]
+
+}
