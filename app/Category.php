7aca902fd1e65169408163422b04d5b091ba1963
islamdiab-stack <islamdiab967@gmail.com>
2021-07-18 06:22:53 +0200

    remove fillable array and use translatable trait and add name in translated attributes

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 
 class Category extends Model
 {
-    protected $fillable = [
-        'name'
-    ];
+    use Translatable;
+    public $translatedAttributes = ['name'];
 }
