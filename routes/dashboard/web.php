259e0ace4608cabf84fe236ee5fa6049ad0aa90c
islamdiab-stack <islamdiab967@gmail.com>
2021-07-18 04:50:25 +0200

    Create new route and add prefix then pass get locale from localization and add middleware like localeSessionRedirect, localizationRedirect, and localeViewPath

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -2,50 +2,61 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('dashboard')->middleware(['auth'])->group(function () {
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
 
-    // Home of dashboard routes
-    Route::get('/home', 'DashboardController@index')->name('dashboard.index');
+        Route::prefix('dashboard')->middleware(['auth'])->group(function () {
 
-    //  Users Routes
-    Route::resource('users', "UserController")->except([
-        'show',
-    ])->parameters([
-        "users" => 'id'
-    ])->names([
-        'index'     => 'users.index',
-        'create'    => 'user.create',
-        'store'     => 'user.store',
-        'edit'      => 'user.edit',
-        'update'    => 'user.update',
-        'destroy'   => 'user.destroy',
-    ]);
+            // Home of dashboard routes
+            Route::get('/home', 'DashboardController@index')->name('dashboard.index');
 
-    //  Categories Routes
-    Route::resource('categories', "CategoryController")->except([
-        'show',
-    ])->parameters([
-        "categories" => 'id'
-    ])->names([
-        'index'     => 'categories.index',
-        'create'    => 'category.create',
-        'store'     => 'category.store',
-        'edit'      => 'category.edit',
-        'update'    => 'category.update',
-        'destroy'   => 'category.destroy',
-    ]);
+            //  Users Routes
+            Route::resource('users', "UserController")->except([
+                'show',
+            ])->parameters([
+                "users" => 'id'
+            ])->names([
+                'index'     => 'users.index',
+                'create'    => 'user.create',
+                'store'     => 'user.store',
+                'edit'      => 'user.edit',
+                'update'    => 'user.update',
+                'destroy'   => 'user.destroy',
+            ]);
 
-    //  Clients Routes
-    Route::resource('clients', "ClientController")->except([
-        'show',
-    ])->parameters([
-        "clients" => 'id'
-    ])->names([
-        'index'     => 'clients.index',
-        'create'    => 'client.create',
-        'store'     => 'client.store',
-        'edit'      => 'client.edit',
-        'update'    => 'client.update',
-        'destroy'   => 'client.destroy',
-    ]);
-}); // End of dashboard routes
+            //  Categories Routes
+            Route::resource('categories', "CategoryController")->except([
+                'show',
+            ])->parameters([
+                "categories" => 'id'
+            ])->names([
+                'index'     => 'categories.index',
+                'create'    => 'category.create',
+                'store'     => 'category.store',
+                'edit'      => 'category.edit',
+                'update'    => 'category.update',
+                'destroy'   => 'category.destroy',
+            ]);
+
+            //  Clients Routes
+            Route::resource('clients', "ClientController")->except([
+                'show',
+            ])->parameters([
+                "clients" => 'id'
+            ])->names([
+                'index'     => 'clients.index',
+                'create'    => 'client.create',
+                'store'     => 'client.store',
+                'edit'      => 'client.edit',
+                'update'    => 'client.update',
+                'destroy'   => 'client.destroy',
+            ]);
+        }); // End of dashboard routes
+
+
+    }
+);
