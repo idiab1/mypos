9ab1fff436e3a6ad64128a828f6c434ff3533a00
islamdiab-stack <islamdiab967@gmail.com>
2021-07-19 07:34:37 +0200

    Add trans method in app where i add tans method in breadcrumb

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -39,7 +39,7 @@
                                 {{-- @include('dashboard.layouts.breadcrumb') --}}
                             <div class="col-sm-6">
                                 <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">{{ trans('site.home') }}</a></li>
                                     {{-- Other Breadcrumb items --}}
                                     @yield('breadcrumb-item')
                                 </ol>
