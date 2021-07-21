3992b49a47b8474fe5d36a701cc05a6c40c4f763
islamdiab-stack <islamdiab967@gmail.com>
2021-07-21 03:11:32 +0200

    Remove timestamps from category_translations table

--- database/migrations/2021_07_18_035037_create_category_translations_table.php
+++ database/migrations/2021_07_18_035037_create_category_translations_table.php
@@ -21,7 +21,6 @@ class CreateCategoryTranslationsTable extends Migration
 
             $table->unique(['category_id', 'locale']);
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
