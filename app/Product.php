8bc70d1daf35c81f7dfcd7122c0f4dc4f27af1e8
islamdiab-stack <islamdiab967@gmail.com>
2021-07-21 04:22:35 +0200

    add guarded type protected and add translate attributes for name, description, then create relationship between products, and category where products belongs to categories

---
+++ app/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+
+class Product extends Model
+{
+    use Translatable;
+
+    protected $guarded = [];
+    public $translatedAttributes = ['name', 'description'];
+
+
+    /**
+     * Get the category that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
