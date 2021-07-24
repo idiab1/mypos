0d3dc7b31a3bc451530694e110cd52da24f91203
islamdiab-stack <islamdiab967@gmail.com>
2021-07-24 12:52:31 +0200

    Add profit_percent in appends array and create new method for calculate profit percent from sale_price / purchase_price for product

--- app/Product.php
+++ app/Product.php
@@ -12,13 +12,20 @@ class Product extends Model
 
     protected $guarded = ['id'];
     public $translatedAttributes = ['name', 'description'];
-    protected $appends = ['image_path'];
+    protected $appends = ['image_path', 'profit_percent'];
 
 
     // Get image path attribute
     public function getImagePathAttribute()
     {
         return asset('uploads/products/' . $this->image);
+    }
+
+    public function getProfitPercentAttribute()
+    {
+        $profit = $this->sale_price - $this->purchase_price;
+        $profit_percent = ($profit * 100) / $this->purchase_price;
+        return number_format($profit_percent, 2);
     }
 
     /**
