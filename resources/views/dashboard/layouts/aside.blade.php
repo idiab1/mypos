c670a8b7ceb44bdab08dedacf91cc5e28e5e93fb
islamdiab-stack <islamdiab967@gmail.com>
2021-10-13 09:22:44 +0200

    remove dashboard tree navbar

--- resources/views/dashboard/layouts/aside.blade.php
+++ resources/views/dashboard/layouts/aside.blade.php
@@ -35,35 +35,6 @@
                 <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Dashboard
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="../../index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v2</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="../../index3.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v3</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link" href="{{route('dashboard.index')}}">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>{{trans('site.dashboard')}}</p>
