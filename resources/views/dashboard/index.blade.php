aa90a5803b16b453045cc4707e81960ec1c6699a
islamdiab-stack <islamdiab967@gmail.com>
2021-10-17 09:20:02 +0200

    Create layout for dashboard home

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,14 +11,88 @@
     {{__('Dashboard')}}
 @endsection
 
-{{-- Breadcrumb Item
-@section('breadcrumb-item')
-    <li class="breadcrumb-item active"><a href="{{route('users.index')}}">Users</a></li>
-@endsection --}}
-
 
 @section('content')
     <div class="home-page">
-        <h2>This is dashboard</h2>
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <div class="small-box bg-dark">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Users</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-users-cog"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="small-box bg-secondary">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Clients</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-user-plus"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>150</h3>
+
+                        <p>New Orders</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-shopping-cart"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>44</h3>
+
+                        <p>Categories</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-layer-group"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="small-box bg-primary">
+                    <div class="inner">
+                        <h3>0</h3>
+
+                        <p>Products</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-clone"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                        More info <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
