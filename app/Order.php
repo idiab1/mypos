bfb6c3baaa188f160c1d6468cdd5ac0c7517a81b
islamdiab-stack <islamdiab967@gmail.com>
2021-10-20 17:22:16 +0200

    Create new function for return all orders where orders belongs to user

--- app/Order.php
+++ app/Order.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $guarded = [];
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+
 }
