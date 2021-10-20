f464725c76a183cc3451abcd05c741cded68d36f
islamdiab-stack <islamdiab967@gmail.com>
2021-10-20 18:06:46 +0200

    create new method for return all productss belongs to orders

--- app/Order.php
+++ app/Order.php
@@ -12,4 +12,8 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function products(){
+        return $this->belongsToMany(Product::class, "product_order");
+    }
+
 }
