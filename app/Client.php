504504f6c802dda5a499b5727d8ed4d8d1986102
islamdiab-stack <islamdiab967@gmail.com>
2021-10-24 18:10:31 +0200

    Create relationship where orders belongs to many clients

--- app/Client.php
+++ app/Client.php
@@ -7,11 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $guarded = [];
+
     protected $casts = [
+
         'phone' => 'array'
+
     ];
 
     protected $fillable = [
         'name', 'phone', 'address'
     ];
+
+    public function orders(){
+
+        return $this->hasMany(Order::class);
+
+    }
 }
