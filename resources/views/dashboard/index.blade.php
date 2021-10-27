3e6030a05affcb0d6b7b1b48b584fd91319bf07d
islamdiab-stack <islamdiab967@gmail.com>
2021-10-27 14:10:32 +0200

    get count rows for users, clients, categories, products, and orders

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -18,7 +18,7 @@
             <div class="col-md-6 col-sm-12">
                 <div class="small-box bg-dark">
                     <div class="inner">
-                        <h3>0</h3>
+                        <h3>{{$users}}</h3>
                         <p>Users</p>
                     </div>
                     <div class="icon">
@@ -32,7 +32,7 @@
             <div class="col-md-6 col-sm-12">
                 <div class="small-box bg-secondary">
                     <div class="inner">
-                        <h3>0</h3>
+                        <h3>{{$clients}}</h3>
                         <p>Clients</p>
                     </div>
                     <div class="icon">
@@ -49,14 +49,14 @@
             <div class="col-md-4 col-sm-12">
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3>{{$orders}}</h3>
 
                         <p>New Orders</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-shopping-cart"></i>
                     </div>
-                    <a href="#" class="small-box-footer">
+                    <a href="{{route("orders.index")}}" class="small-box-footer">
                         More info <i class="fas fa-arrow-circle-right"></i>
                     </a>
                 </div>
@@ -64,7 +64,7 @@
             <div class="col-md-4 col-sm-12">
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3>0</h3>
+                        <h3>{{$categories}}</h3>
                         <p>Categories</p>
                     </div>
                     <div class="icon">
@@ -78,7 +78,7 @@
             <div class="col-md-4 col-sm-12">
                 <div class="small-box bg-primary">
                     <div class="inner">
-                        <h3>0</h3>
+                        <h3>{{$products}}</h3>
                         <p>Products</p>
                     </div>
                     <div class="icon">
