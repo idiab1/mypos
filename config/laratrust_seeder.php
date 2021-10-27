6bbfc8d20bd43109483b89247bccdcd71e571821
islamdiab-stack <islamdiab967@gmail.com>
2021-10-27 06:55:18 +0200

    Add orders cread, read, update, and delete on permissions role

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -17,8 +17,10 @@ return [
             'categories' => 'c,r,u,d',
             'clients' => 'c,r,u,d',
             'products' => 'c,r,u,d',
+            'orders' => 'c,r,u,d',
         ],
         'admin' => [],
+
 
     ],
 
