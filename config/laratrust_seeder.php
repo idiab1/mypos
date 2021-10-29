2d28758760bf5310dba1946070df2d620e58e620
islamdiab-stack <islamdiab967@gmail.com>
2021-10-29 10:08:03 +0200

    update

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -14,8 +14,8 @@ return [
     'roles_structure' => [
         'super_admin' => [
             'users' => 'c,r,u,d',
-            'categories' => 'c,r,u,d',
             'clients' => 'c,r,u,d',
+            'categories' => 'c,r,u,d',
             'products' => 'c,r,u,d',
             'orders' => 'c,r,u,d',
         ],
