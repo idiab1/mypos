52aa9afc70dee6f4c5ce797b0542096220e9e60f
islamdiab-stack <islamdiab967@gmail.com>
2021-11-03 17:19:34 +0200

    remove timestamps method

--- database/migrations/2021_10_20_152729_create_product_order_table.php
+++ database/migrations/2021_10_20_152729_create_product_order_table.php
@@ -18,7 +18,6 @@ class CreateProductOrderTable extends Migration
             $table->integer("product_id")->unsigned();
             $table->integer("order_id")->unsigned();
             $table->integer("quantity")->default(1);
-            $table->timestamps();
 
             $table->foreign("product_id")->references("id")->on("products")->onDelete("cascade");
             $table->foreign("order_id")->references("id")->on("orders")->onDelete("cascade");
