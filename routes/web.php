08480f27b3a42b9c8b663e5d125ae0645aecc8a9
islamdiab-stack <islamdiab967@gmail.com>
2022-06-13 03:34:24 +0300

    redirect home route to dashboard homepage

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+// Route::get('/', 'HomeController@index')->name('home');
+
+Route::redirect('/', 'dashboard/home');
+// Route::get('/', 'HomeController@index')->name('home');
