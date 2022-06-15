c54c7838b3e207d03c31294dd47a509c7c5943ed
islamdiab-stack <islamdiab967@gmail.com>
2022-06-15 19:34:56 +0300

    update

--- public/js/app.js
+++ public/js/app.js
@@ -37392,10 +37392,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/dashboard/app.scss":
+/*!*******************************************!*\
+  !*** ./resources/sass/dashboard/app.scss ***!
+  \*******************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -37404,14 +37404,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!***********************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/dashboard/app.scss ***!
+  \***********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\xampp\htdocs\laravel_projects\mypos\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel_projects\mypos\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel_projects\mypos\resources\sass\dashboard\app.scss */"./resources/sass/dashboard/app.scss");
 
 
 /***/ })
