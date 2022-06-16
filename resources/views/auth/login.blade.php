ddb5b5f857dc604841dfcd37e6aab2348bf1357b
islamdiab-stack <islamdiab967@gmail.com>
2022-06-17 00:20:27 +0300

    change the layout of login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,75 +4,102 @@
 @section('navbar') @endsection
 
 @section('content')
+<!-- Sign Page -->
 <section class="login-page sign-page">
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+        <div class="row m-0">
+            <div class="col-10 m-auto">
+                <!-- Sign Content -->
+                <div class="sign-content">
+                    <div class="row justify-content-centr m-auto">
+                        <div class="col-md-5 p-0">
+                            <!-- Sign info -->
+                            <div class="card sign-info">
+                                <div class="card-body">
+                                    <h3 class="m-0">Logo</h3>
                                 </div>
                             </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                            <!-- End of Sign info -->
+                        </div>
+                        <div class="col-md-7 p-0">
+                            <!-- Sign Form -->
+                            <div class="card sign-form">
+                                <!-- Card Header -->
+                                <div class="card-header text-center">
+                                    <h4>{{ __('Login') }}</h4>
                                 </div>
-                            </div>
+                                <!-- End of Card Header -->
 
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
 
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
+                                        <!-- Email -->
+                                        <div class="form-group">
+                                            <label for="email" class="text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <!-- End of Email -->
+
+                                        <!-- Password -->
+                                        <div class="form-group">
+                                            <label for="password" class="text-md-right">
+                                                {{ __('Password') }}
+                                            </label>
+
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <!-- End of Password -->
+
+
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group mb-0">
+                                            <button type="submit" class="btn btn-primary btn-crayons btn-login">
+                                                {{ __('Login') }}
+                                            </button>
+
+                                            {{-- @if (Route::has('password.request'))
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif --}}
+                                        </div>
+                                    </form>
                                 </div>
+                                <!-- End of Card Body -->
                             </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
+                            <!-- End of Sign Form -->
+                        </div>
                     </div>
                 </div>
+                <!-- End of Sign Content -->
             </div>
         </div>
     </div>
 </section>
+<!-- End of Sign Page -->
 @endsection
