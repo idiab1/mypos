6926388b44fa0ed5ec9d5ba0d252eec64b67dcac
islamdiab-stack <islamdiab967@gmail.com>
2022-06-18 05:28:57 +0300

    update

--- resources/views/dashboard/layouts/aside.blade.php
+++ resources/views/dashboard/layouts/aside.blade.php
@@ -2,18 +2,20 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a class="brand-link" href="{{route('dashboard.index')}}">
-        <span class="brand-text font-weight-bold">POS</span>
+        <span class="font-weight-bold">MyPos</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                {{-- <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> --}}
-            </div>
+            {{-- <div class="image">
+                <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div> --}}
             <div class="info">
-                <a href="#" class="d-block">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</a>
+                <a href="#" class="d-block">
+                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
+                </a>
             </div>
         </div>
 
