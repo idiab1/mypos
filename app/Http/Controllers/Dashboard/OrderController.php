9266ceaca1a7c2b6010b224c8649d826b01fa717
islamdiab-stack <islamdiab967@gmail.com>
2022-06-23 09:29:38 +0300

    return all orders from databases

--- app/Http/Controllers/Dashboard/OrderController.php
+++ app/Http/Controllers/Dashboard/OrderController.php
@@ -15,7 +15,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::paginate(5);
+        $orders = Order::all();
         return view("dashboard.orders.index", compact("orders"));
     }
 
